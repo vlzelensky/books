f234f587c7613e848b632eb97bbeb4c5385643f5
vlzelensky <vzelensky.exceedteam@gmail.com>
2022-08-15 18:04:51 +0300

    feat/test commit

--- src/Components/Books/Books.tsx
+++ src/Components/Books/Books.tsx
@@ -3,7 +3,6 @@ import { observer } from 'mobx-react-lite';
 import { library } from 'store';
 import { useLazyLoad } from 'hooks';
 import { Book } from './modules';
-
 import './styles.css';
 
 export const Books = observer(() => {
