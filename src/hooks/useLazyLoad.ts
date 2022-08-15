6f7a24a5c2e639d1ca96cfd262ca5774fcff44f8
vlzelensky <vzelensky.exceedteam@gmail.com>
2022-08-15 10:27:19 +0300

    feat/ lazy load hook,

--- src/hooks/useLazyLoad.ts
+++ src/hooks/useLazyLoad.ts
@@ -5,10 +5,10 @@ export const useLazyLoad = (elements: BookI[], elementsPerPage: number) => {
   const [data, setData] = useState<BookI[]>(elements.slice(0, elementsPerPage));
 
   const onScroll = (event: SyntheticEvent<HTMLDivElement>) => {
-    if (
-      event.currentTarget.scrollHeight - event.currentTarget.scrollTop ===
-      event.currentTarget.clientHeight
-    ) {
+    const {
+      currentTarget: { scrollHeight, clientHeight, scrollTop },
+    } = event;
+    if (scrollHeight - scrollTop === clientHeight) {
       if (data.length !== elements.length) {
         setData((prevState) => [
           ...prevState,
