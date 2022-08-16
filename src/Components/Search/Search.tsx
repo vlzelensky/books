06645da23675df1a5662c3f8047ee776e767ea17
vlzelensky <vzelensky.exceedteam@gmail.com>
2022-08-16 10:20:19 +0300

    feat/resolve merge conflicts

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,11 +1,11 @@
 import { FC, ChangeEvent } from 'react';
 import './styles.css';
 import { observer } from 'mobx-react-lite';
-import { books } from '../../store';
+import { library } from '../../store';
 
 export const Search: FC = observer(() => {
   const onChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
-    books.filterBooks(value);
+    library.filterBooks(value);
   };
 
   return (
