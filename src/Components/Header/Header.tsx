9422d3d261479a3d69c2d2e9dfdcb3f1b7b2cdae
vlzelensky <vzelensky.exceedteam@gmail.com>
2022-08-16 15:26:54 +0300

    feat/remove Search component, add reusable Input component

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
 import { observer } from 'mobx-react-lite';
-import { Search } from 'Components';
+import { Input } from 'Components';
 import { convertTakenBooksAmount } from 'utlis/convertTakenBooksAmount';
 import { library } from 'store';
 import './styles.css';
 
 export const Header = observer(() => {
+  const [searchValue, setSearchValue] = useState<string>('');
+
+  const onChange = (value: string) => setSearchValue(value);
+
   const booksTaken = convertTakenBooksAmount(library.booksTaken);
   return (
     <div className='header'>
       <span className='header_counter'>У вас на руках: {booksTaken}</span>
-      <Search />
+      <Input
+        type='text'
+        placeholder='Поиск книги'
+        value={searchValue}
+        onChange={onChange}
+      />
     </div>
   );
 });
