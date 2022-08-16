ac9f151615540797d83ee1937ccccd646f080a52
vlzelensky <vzelensky.exceedteam@gmail.com>
2022-08-16 11:20:04 +0300

    feat/create redirection to book page

--- src/pages/BookPage/BookPage.tsx
+++ src/pages/BookPage/BookPage.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { observer } from 'mobx-react-lite';
+import { library } from 'store';
+import { Book } from '../MainPage/modules';
 
-export const BookPage = () => {
-  const { id } = useParams();
-  return <div></div>;
-};
+export const BookPage = observer(() => {
+  const { id: paramsId } = useParams();
+  const book = library.books.find((book) => book.id === Number(paramsId));
+  if (!book) {
+    return <div>404</div>;
+  } else {
+    const { id, author, name, image, isTaken, description } = book;
+    return (
+      <div className='container'>
+        <Book id={id} author={author} name={name} image={image} isTaken={false} />
+      </div>
+    );
+  }
+});
--- src/pages/MainPage/modules/Book/Book.tsx
+++ src/pages/MainPage/modules/Book/Book.tsx
@@ -1,11 +1,17 @@
 import React, { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { BookProps } from './types';
 import '../../styles.css';
 
-export const Book: FC<BookProps> = ({ id, author, name, image, isTaken }) => (
-  <div className={`book ${isTaken && 'book_isTaken'}`} key={id}>
-    <img className={`book_image ${isTaken && 'isTaken'}`} src={image} alt={name} />
-    <span className={`book_author ${isTaken && 'isTaken'}`}>{author}</span>
-    <span className={`book_name ${isTaken && 'isTaken'}`}>{name}</span>
-  </div>
-);
+export const Book: FC<BookProps> = ({ id, author, name, image, isTaken }) => {
+  const navigate = useNavigate();
+  const onClick = () => navigate(`/books/${id}`);
+
+  return (
+    <div className={`book ${isTaken && 'book_isTaken'}`} key={id} onClick={onClick}>
+      <img className={`book_image ${isTaken && 'isTaken'}`} src={image} alt={name} />
+      <span className={`book_author ${isTaken && 'isTaken'}`}>{author}</span>
+      <span className={`book_name ${isTaken && 'isTaken'}`}>{name}</span>
+    </div>
+  );
+};
